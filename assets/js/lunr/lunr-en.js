b04709fb9a0858a2e9080ed5048010dcb7a64dac
Ricardo Adao <radao@radao-a01.vmware.com>
2019-11-13 15:44:06 +0000

    - mmistakes update

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -48,9 +48,9 @@ $(document).ready(function() {
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+              
+              '</h2>'+
               '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="" style="max-width: 150px">'+
+                '<img src="'+store[ref].teaser+'" alt="">'+
               '</div>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
