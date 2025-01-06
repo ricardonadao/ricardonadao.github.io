170b420bcff1de13a36eb180b6bed5a124125798
Ricardo Adao <ricardo.adao@gmail.com>
2025-01-06 17:21:44 +0000

    - New update

--- _posts/2025-01-06-configure-syslog-server-multiple-esxi.md
+++ _posts/2025-01-06-configure-syslog-server-multiple-esxi.md
@@ -1,8 +1,8 @@
 ---
 author: Ricardo Adao
 published: true
-date: 2018-07-15 13:20:17
-
+date: 2025-01-06 17:00:00
+last_modified_at: null
 header:
   teaser: /assets/images/featured/powercli-150x150.png
 title: PowerCLI - Configure syslog server in multiple ESXi
@@ -17,10 +17,13 @@ tags:
   - vmware
   - syslog
 toc: true
+draft: false
+mathjax: false
 slug: powercli-configure-syslog-server-multiple-esxi
-last_modified_at: 2023-06-21T08:14:48.833Z
 ---
 This is a quick powershell script to setup the _remote syslog_ in all the hosts of a cluster or vCenter.
+
+Normally this task is simplified when using the integration of _VMware Log Insight/VMware Aria Operations for Logs (VRLI)_, however this options sometimes is not always available, for example in the extreme cases where a need of an *External Load Balancer* needs to be used for _VRLI_ or if we are not even using it as our logging collector, hence sometimes we could need to setup the syslog for multiple _ESXi_ in one go.
 
 # Script parameters #
 
@@ -68,4 +71,4 @@ $vmHosts | ForEach-Object {
 }
 ```
 
-## _Github_ Link for the entire script **>>** [set.esxi.syslog.ps1](https://github.com/ricardonadao/vrandombites.co.uk/blob/master/ESXi/set.esxi.syslog.ps1) ##
+## _Github_ Link for the entire script **>>** [set.esxi.syslog.ps1]({{ relative_url }}/assets/downloads/scripts/powershell/esxi/set.esxi.syslog.ps1) ##
---
+++ assets/downloads/scripts/powershell/esxi/set.esxi.syslog.ps1
@@ -0,0 +1,122 @@
+﻿##################################################################
+#
+# Setup remote syslog in all the hypervisors of a Cluster
+#
+##################################################################
+<#
+.SYNOPSIS
+    Change the remote syslog setting in all the hypervisors of a cluster
+
+.DESCRIPTION
+    Change the remote syslog setting in all the hypervisors of a cluster
+
+.PARAMETER vcenter
+Specifies FQDN/IP of the vCenter
+
+.PARAMETER vcenterUsername
+Specifies username to use to login to the vCenter
+
+.PARAMETER vcenterPassword
+Specifies password for vCenterUsername (passed encrypted as a parameter or leave it blank to be asked)
+
+.PARAMETER cluster
+Specifies the cluster containing the hypervisors to be reconfigured (optional)
+
+.PARAMETER remoteSyslog
+Specify FQDN/IP of remote syslog to configure
+
+.PARAMETER syslogPort
+Specify syslog Port to use (default: 514) (optional)
+
+
+.EXAMPLE
+    C:\PS>
+    . set.esxi.syslog.ps1 -vcenter <vcenter IP/FQDN> -vcenterUsername <vcenter username>
+       -vcenterPassword <password> -cluster <clusterName> -remoteSyslog <syslog FQDN/IP>
+       -syslogPort <port to use>
+.NOTES
+    Author: Ricardo Adao
+    Date:   Jul 15, 2018
+#>
+
+param(
+    [Parameter(Position=1, Mandatory = $true, ValueFromPipeline = $true)] [string]$vcenter,
+    [Parameter(Position=2, Mandatory = $true, ValueFromPipeline = $true)] [string]$vcenterUsername,
+    [Parameter(Position=3, Mandatory = $true, ValueFromPipeline = $true)] [System.Security.SecureString]$vcenterPassword,
+    [Parameter(Position=4, Mandatory = $true, ValueFromPipeline = $true)] [string]$remoteSyslog,
+    [Parameter(Position=5, Mandatory = $false, ValueFromPipeline = $true)] [string]$syslogPort = 514,
+    [Parameter(Position=6, Mandatory = $false, ValueFromPipeline = $true)] [string]$cluster
+)
+
+# Connect to vCenter
+if (!($vcenterConnection = Connect-VIServer  -Server $vcenter -User $vcenterUsername -Password ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($vcenterPassword))) -ErrorAction SilentlyContinue)) { #DevSkim: ignore DS104456 
+    $exceptionError = $Error[0]
+    switch ($exceptionError.CategoryInfo.Category) {
+        "ObjectNotFound" {
+            switch ($exceptionError.CategoryInfo.Reason) {
+                "ViServerConnectionException" {
+                    Write-Host "=> set.esxi.power.settings -> vCenter not found - $vcenter <=" -ForegroundColor Red
+                }
+                default {
+                    Write-Host "=> set.esxi.power.settings -> ObjectNotFound - Default - vcenter: $vcenter <=" -ForegroundColor Red
+                }
+            }
+            Write-Host "=> Error Category:" $exceptionError.CategoryInfo.Category -ForegroundColor Yellow
+            Write-Host "=> Error Message:" $exceptionError.Message -ForegroundColor Yellow
+            Write-Host ""
+            Exit
+        }
+        "NotSpecified" {
+            switch ($exceptionError.CategoryInfo.Reason) {
+                "InvalidLogin" {
+                    Write-Host "=> set.esxi.power.settings -> Invalid Credentials - vcenter: $vcenter - username: $username <=" -ForegroundColor Red
+                }
+                default {
+                    Write-Host "=> set.esxi.power.settings -> NotSpecified - Default - vcenter: $vcenter <=" -ForegroundColor Red
+                }
+            }
+            Write-Host "=> Error Category:" $exceptionError.CategoryInfo.Category -ForegroundColor Yellow
+            Write-Host "=> Error Reason:" $exceptionError.CategoryInfo.Reason -ForegroundColor Yellow
+            Write-Host "=> Error Message:" $exceptionError.Exception.Message -ForegroundColor Yellow
+            Exit
+        }   
+        default {
+            Write-Host "=> set.esxi.power.settings -> Default - $vcenter <=" -ForegroundColor Red
+            Write-Host "=> Error Category:" $exceptionError.CategoryInfo.Category -ForegroundColor Yellow
+            Write-Host "=> Error Reason:" $exceptionError.CategoryInfo.Reason -ForegroundColor Yellow
+            Write-Host "=> Error Message:" $exceptionError.Exception.Message -ForegroundColor Yellow
+            Exit
+        }
+    }
+}
+
+# Show the current config
+Write-Host "--> Current Settings:`n"
+if (!$cluster) {
+    $vmHosts = Get-VMHost | Sort-Object $_.Name
+} else {
+    $vmHosts = Get-Cluster -Name $cluster | Get-VMHost | Sort-Object $_.Name
+}
+
+# Show current config
+$vmHosts | ForEach-Object {
+    Write-Host $_.Name
+    Get-VMHostSysLogServer -VMHost $_
+}
+
+# Set syslog config in hypervisors
+$vmHosts | ForEach-Object {
+    Write-Host $_.Name
+    Set-VMHostSysLogServer -SysLogServer $remoteSyslog":"$syslogPort -VMHost $_
+}
+
+# Restart syslog and set the allow rules in the ESXi
+$vmHosts | ForEach-Object {
+    Write-Host $_.Name
+    (Get-Esxcli -v2 -VMHost $_).system.syslog.reload.Invoke()
+    (Get-Esxcli -v2 -VMHost $_).network.firewall.ruleset.set.Invoke(@{rulesetid='syslog'; enabled=$true})
+    (Get-Esxcli -v2 -VMHost $_).network.firewall.refresh.Invoke()
+}
+
+
+Disconnect-viserver -Server $vcenter -Confirm:$false -ErrorAction SilentlyContinue
